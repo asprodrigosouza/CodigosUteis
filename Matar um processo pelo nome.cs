c0ebe4f9825bee75fa310ced1bea103404787a9e
asprodrigosouza <tenrodrigo@yahoo.com.br>
2022-02-17 19:26:53 -0300

    Update Matar um processo pelo nome.cs

--- Matar um processo pelo nome.cs
+++ Matar um processo pelo nome.cs
@@ -1,9 +1,13 @@
+//Método para matar um processo pelo nome
 private static void FinalizarProcesso()
 {
+    //Lista os processos existentes
     Process[] listProcesso = Process.GetProcesses();
     foreach (Process process in Process.GetProcessesByName("wkhtmltopdf"))
     {
+        //E a partir do nome selecionado no foreach, mata o mesmo.
         process.Kill();
     }
+    //Aguarda um tempo determinado para executar novamente.
     System.Threading.Thread.Sleep(500);
 }
