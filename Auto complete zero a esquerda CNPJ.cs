b6dc0c8a01d3782eea74dec0ebed821197b595d7
asprodrigosouza <tenrodrigo@yahoo.com.br>
2022-02-17 18:34:17 -0300

    Codigos Uteis
    PadLeft, PadRight

---
+++ Auto complete zero a esquerda CNPJ.cs
@@ -0,0 +1,7 @@
+string CNPJ = "123";
+
+//Completa com zeros a esquerda até o CNPJ atingir 9 números.
+CNPJ.PadLeft(9, '0');
+
+//Completa com zeros a direita até o CNPJ atingir 9 números.
+CNPJ.PadRight(9, '0');
