10422c26466f26d66969aae17ddf25c657a59af4
asprodrigosouza <tenrodrigo@yahoo.com.br>
2022-02-17 19:12:38 -0300

    Update Removendo caracteres com Replace.cs

--- Removendo caracteres com Replace.cs
+++ Removendo caracteres com Replace.cs
@@ -13,3 +13,16 @@ fraseExemplo.Replace(".", "");
 
 //Remover tudo de uma vez.
 fraseExemplo.Replace(",", "").Replace(";", "").Replace(".", "");
+
+
+
+//Você pode criar um método que faça isso e só chamar quando precisar, para organizar melhor seu código.
+
+public static String RemoverPontoVigula(String str)
+{
+    str.Replace(",", "").Replace(".", "");
+}
+
+//Exemplo de uso
+fraseExemplo = RemoverPontoVigula(fraseExemplo);
+//A frase ficara: "Hoje eu vou para casa; Mas vamos passear antes"
