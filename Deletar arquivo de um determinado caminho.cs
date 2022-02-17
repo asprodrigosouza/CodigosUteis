9f2050178532349a31f8849d10d619dfdee5049a
asprodrigosouza <tenrodrigo@yahoo.com.br>
2022-02-17 19:24:05 -0300

    Update Deletar arquivo de um determinado caminho.cs

--- Deletar arquivo de um determinado caminho.cs
+++ Deletar arquivo de um determinado caminho.cs
@@ -1,7 +1,9 @@
+//Método para deletar um arquivo local
 private static void DeleteFile(string path)
 {
     try
     {
+        //Se existe o arquivo no local informado, deleta
         if(File.Exists(path))
         {
             File.Delete(path);
@@ -10,8 +12,9 @@ private static void DeleteFile(string path)
         }
     }
     catch (Exception ex)
-    {
-        
+    {        
         throw ex;
+        //Sempre que possivel é bom no catch criar um log do erro e salvar o mesmo.
+        Log.Error($"Erro ao deletar o arquivo, erro: {ex}");
     }
 }
